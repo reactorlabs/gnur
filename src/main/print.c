39a7eaaf90991c646cdeb1ec12c8a9f04ddf6253
oli <o@o1o.ch>
2021-07-09 12:33:02 +0000

    protect bug

--- src/main/print.c
+++ src/main/print.c
@@ -499,7 +499,7 @@ static void PrintGenericVector(SEXP s, R_PrintData *data)
 	    printArray(t, dims, 0, Rprt_adj_left, names);
 	    UNPROTECT(1);
 	}
-	UNPROTECT(1);
+	UNPROTECT(2);
     }
     else { // no dim()
 	PROTECT(names = getAttrib(s, R_NamesSymbol));
